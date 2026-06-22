3e2629ddeba668190d0c8c7cf0890049580482d9
admin <admin@LAPTOP-MDU6CIMR>
2026-06-22 14:54:55 +0800

    11

--- assignment1-basics-main/cs336_basics/model/tokenizer_experiment.py
+++ assignment1-basics-main/cs336_basics/model/tokenizer_experiment.py
@@ -4,7 +4,7 @@ import random
 import numpy as np
 from typing import Iterator, TextIO
 
-from cs336_basics.tokenizer import Tokenizer
+from cs336_basics.model import Tokenizer
 
 EOT = "<|endoftext|>"
 
